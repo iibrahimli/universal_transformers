c5891adc0f530d82777d37a26e654a173eabad03
iibrahimli <imranibrahimli98@gmail.com>
2022-06-05 15:42:16 +0200

    remove unused function

--- utils/utils.py
+++ utils/utils.py
@@ -23,44 +23,6 @@ def prepare_target(labels: Tensor, attention_mask: Tensor, decoder_start_token_i
     return shifted_labels, shifted_attn_mask
 
 
-def translate_tokens(
-    input_ids: Tensor,
-    model: nn.Module,
-    tokenizer,
-    device: str = "cpu",
-    trim: bool = True,
-):
-    """
-    Translate tokens.
-    """
-
-    if input_ids.dim() == 1:
-        input_ids = input_ids.unsqueeze(0)
-    input_ids = input_ids.to(device)
-
-    if trim:
-        # remove trailing eos tokens (if any)
-        for last_id in range(input_ids.shape[1] - 1, -1, -1):
-            if input_ids[0, last_id] != tokenizer.eos_token_id:
-                break
-        last_id += 1
-
-    with torch.no_grad():
-        out = (
-            model.generate(
-                input_ids[0, :last_id],
-                eos_token_id=tokenizer.eos_token_id,
-                min_length=2,
-                max_length=100,
-            )
-            .squeeze()
-            .detach()
-            .cpu()
-        )
-
-    return out
-
-
 def translate_text(source: str, model: nn.Module, tokenizer, device: str = "cpu"):
     """
     Translate a text.
