2c5ee7b20e6b3ec5427ea45508851bd9cb5dc194
iibrahimli <imranibrahimli98@gmail.com>
2022-06-05 12:57:02 +0200

    add betas to Adam

--- train_wmt14.py
+++ train_wmt14.py
@@ -279,7 +279,7 @@ if __name__ == "__main__":
 
     # Training extras
     loss = torch.nn.CrossEntropyLoss(label_smoothing=args.label_smoothing).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.98))
     scheduler = utils.CustomLRScheduler(
         optimizer, d_model=args.d_model, warmup_steps=5000, lr_mul=1.0
     )
